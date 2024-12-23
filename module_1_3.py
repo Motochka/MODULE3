42f8ea211ab7f8e8b2b895bf8906709cbfa16cd7
Motochka <motochka111@gmail.com>
2024-12-23 11:58:47 -0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Tanya'
+print (name)
+age=44
+print(age)
+age=44+2
+print(age)
+is_student=(age ==44 or age==46)
+if is_student:
+    print('Is Student:', True)
+else:
+    print('Is Student:', False)
